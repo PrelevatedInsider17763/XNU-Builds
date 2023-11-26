9c4f12d2909728aae859ae7244a42c94fe05320f
MemberMaddenInsiderDev25120 <sreeram.sathya2014@outlook.com>
2023-11-26 09:17:34 +0530

    Update fetch.sh

--- fetch.sh
+++ fetch.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # XNU Kernel Fetch
 echo Fetching XNU kernel...
-git clone https://github.com/Apple-OSS-Distributions/XNU
+git clone https://github.com/Apple-OSS-Distributions/XNU src
 echo Fetch completed.
